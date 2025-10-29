b8668352cf240798e9a15d0fe9ca0a1e5f4e76bd
math92n4 <math92n4@stud.kea.dk>
2025-10-29 13:25:33 +0100

    init

---
+++ src/main/java/com/example/nordicelectronics/NordicElectronicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.nordicelectronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordicElectronicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NordicElectronicsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nordicelectronics/NordicElectronicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nordicelectronics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordicElectronicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
