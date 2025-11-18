92eb8382d3f44ff4249d224eb5dc2511e125f6c7
math92n4 <math92n4@stud.kea.dk>
2025-11-18 17:09:38 +0100

    Testing the workflow

--- src/test/java/com/example/nordicelectronics/SampleTestForTest.java
+++ src/test/java/com/example/nordicelectronics/SampleTestForTest.java
@@ -2,18 +2,9 @@ package com.example.nordicelectronics;
 
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
-@ActiveProfiles("test")
-// excluding db stuff for now, make h2 later
-@EnableAutoConfiguration(exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
-})
 public class SampleTestForTest {
 
     @Test
