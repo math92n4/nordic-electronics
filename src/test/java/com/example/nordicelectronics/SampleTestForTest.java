c6aac8052b781970fda19fb8ac573cee7e28c0e0
math92n4 <math92n4@stud.kea.dk>
2025-11-18 16:57:51 +0100

    Testing the workflow

--- src/test/java/com/example/nordicelectronics/SampleTestForTest.java
+++ src/test/java/com/example/nordicelectronics/SampleTestForTest.java
@@ -2,9 +2,13 @@ package com.example.nordicelectronics;
 
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
+@ActiveProfiles("test")
 public class SampleTestForTest {
 
     @Test
